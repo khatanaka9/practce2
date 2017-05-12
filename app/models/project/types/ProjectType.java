c815ab57a475baa2100fc685f68ecfb32c7958bd
harita <hiroaki.harita@soliton.co.jp>
2017-05-12 18:27:45 +0900

    hharita test push

--- app/models/project/types/ProjectType.java
+++ app/models/project/types/ProjectType.java
@@ -1,33 +1,30 @@
 package models.project.types;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
+import lombok.*;
+import lombok.experimental.*;
 
 @Getter
-@Setter
 @Accessors(fluent = true)
 public enum ProjectType {
-
-	不動産案件(1,true),
-	銀行システム案件(2,true),
-	病院システム案件(3,true),
-	;
-	public Integer intValue;
-	public Boolean ediType;
-
-	private ProjectType(Integer intValue,Boolean ediType){
-		this.intValue = intValue;
-		this.ediType = ediType;
-	}
-
-	public static ProjectType valueOf(final Integer value){
-		for (final ProjectType type : values()) {
+    
+    不動産案件(1, true),
+    銀行システム案件(2, true),
+    病院システム案件(3, true), ;
+    public Integer intValue;
+    public Boolean ediType;
+    
+    private ProjectType(final Integer intValue, final Boolean ediType) {
+        this.intValue = intValue;
+        this.ediType = ediType;
+    }
+    
+    public static ProjectType valueOf(final Integer value) {
+        for (final ProjectType type : values()) {
             if (type.intValue.equals(value)) {
                 return type;
             }
         }
-
-       return null;
+        
+        return null;
     }
 }
