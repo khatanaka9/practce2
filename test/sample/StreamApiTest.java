2ee581ef8a5239ed75ca97dd26175f073a06a0a1
khatanaka9 <hatanaka@inazou.jp>
2017-05-19 16:44:05 +0900

    importついか

--- test/sample/StreamApiTest.java
+++ test/sample/StreamApiTest.java
@@ -1,13 +1,16 @@
 package sample;
 
+import org.junit.*;
+
+import play.test.*;
 
 //機能毎にファイル作成したのでDeprecated
 @Deprecated
 public class StreamApiTest extends UnitTest {
-    
-    @Before
-    public void setUp() {
-        Fixtures.deleteDatabase();
-        
-    }
+
+	@Before
+	public void setUp() {
+		Fixtures.deleteDatabase();
+
+	}
 }
