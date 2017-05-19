24bdb4fd966bef65eab4c3e4452d2296d82a07fb
khatanaka9 <hatanaka@inazou.jp>
2017-05-19 15:34:41 +0900

    三項演算子test

--- test/sample/SkipTest.java
+++ test/sample/SkipTest.java
@@ -1,5 +1,33 @@
 package sample;
 
-public class SkipTest {
+import static org.hamcrest.CoreMatchers.*;
 
+import java.util.*;
+import java.util.stream.*;
+
+import org.junit.*;
+
+import play.test.*;
+
+public class SkipTest extends UnitTest {
+
+	@Before
+	public void setUp() {
+		Fixtures.deleteDatabase();
+	}
+
+	// skip
+	@Test
+
+	public void SkipTest() {
+
+		final List<String> list = Arrays.asList("A", "BB", "CCC", "DDDD", "EEEEE", "FFFFFF");
+
+		final List<String> skip = list.stream().limit(3).collect(Collectors.toList());
+
+		// 先頭から3件飛んでいるはずと
+		System.out.println(skip);
+		// 3件存在しているはず
+		assertThat(skip.size(), is(3));
+	}
 }
