b0fd41e195e8214a016b009e097d394a178f3e5e
harita <hiroaki.harita@soliton.co.jp>
2017-05-19 16:13:58 +0900

    hharita 指摘点の追加

--- test/sample/sankou.java
+++ test/sample/sankou.java
@@ -1,23 +1,25 @@
 package sample;
 
-import org.junit.*;
-
-import play.test.*;
-
 public class sankou extends UnitTest {
-
-	@Test
-	public void Sankou() {
-
-		final int x;
-		int y;
-		final char z;
-
-		x = 0;
-		y = 10;
-		z = x < y ? '真' : '偽';
-		// z=真になるはず
-		System.out.println(z);
-	}
-
+//TODO hatanaka クラス名は大文字
+    
+    @Test
+    public void Sankou() {
+        
+        //TODO hatanaka プリミティブ型は使わない(boolean以外)
+        //TODO hatanaka intフィールド片方だけfinalがついててもう片方がfinalがついてない理由はなぜでしょう
+        //   -> finalの意味がわからなければ調査すること
+        final int x;
+        int y;
+        final char z;
+        
+        x = 0;
+        y = 10;
+        z = x < y
+                ? '真'
+                : '偽';
+        // z=真になるはず
+        System.out.println(z);
+    }
+    
 }
